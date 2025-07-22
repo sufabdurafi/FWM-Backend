3101dd6f71db025e3a6970485d48d7fe15391bc5
alfrxyx <alfarabigajali@gmail.com>
2025-07-22 12:56:41 +0700

    Update CORS configuration to allow requests from the new production domain

--- config/cors.php
+++ config/cors.php
@@ -2,7 +2,7 @@
 return [
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['http://localhost:5173'],
+    'allowed_origins' => ['http://localhost:5173', 'https://flywithme.vercel.app'],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
